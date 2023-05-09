252978c09be89163976dd1d8a43372c364c9a289
João Seixas <joao.seixas@aluno.ufabc.edu.br>
2023-05-09 15:03:52 -0300

    Controller de tags adicionado, além disso, webpacker e react instalados

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,34 @@
 class TagsController < ApplicationController
+      skip_before_action :verify_authenticity_token
+  
+    def index
+        @tags = Tag.all
+        render json: @tags
+    end
+  
+    def show
+      @tag = Tag.find(params[:id])
+      render json: @tag
+    end
+  
+    def update
+      @tag = Tag.find(params[:id])
+    end
+  
+    def create
+      tag = Tag.new(tag_params)
+      if Tag.exists?(name: tag.name)
+        render json: { error: "Tag name already registered" }, status: :unprocessable_entity
+      elsif tag.save
+        render json: tag, status: :created
+      else
+        render json: tag.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+    
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
 end
