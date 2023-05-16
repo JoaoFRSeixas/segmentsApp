d0f8c4807113efcf7e76cd674e2ad18f7926b132
João Seixas <joao.seixas@aluno.ufabc.edu.br>
2023-05-16 17:38:34 -0300

    Relação entre tags e users criada e adicionada no arquivo de seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :tags, class_name: "Tag"
   has_many :user_segments, class_name: "UserSegments"
   has_many :segment, through: :user_segments, class_name: "Segment"
 end
--- db/migrate/20230516184540_add_ref_to_users_and_segments.rb
+++ db/migrate/20230516184540_add_ref_to_users_and_segments.rb
@@ -1,7 +1,7 @@
 class AddRefToUsersAndSegments < ActiveRecord::Migration[7.0]
   def change
     add_reference :user_segments, :user, null: false, foreign_key: true
-    add_reference :user_segments, :segments, null: false, foreign_key: true
+    add_reference :user_segments, :segment, null: false, foreign_key: true
 
   end
 end
